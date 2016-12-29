5abb14f71b7ca33e846210425b5de34904aba7e7
Luther <lutheroto@gmail.com>
2016-12-29 04:40:19 -0500

    Implement OpenAddressing

--- 01-data-structures/05-hashes-part-2/open_addressing/open_addressing.rb
+++ 01-data-structures/05-hashes-part-2/open_addressing/open_addressing.rb
@@ -2,29 +2,72 @@ require_relative 'node'
 
 class OpenAddressing
   def initialize(size)
+    @items = Array.new(size)
+    @item_count = 0
   end
 
   def []=(key, value)
+    i = index(key)
+    while @items[i] && @items[i].key != key
+      i = (i + 1) % size
+    end
+    if @items[i]
+      @items[i].value = value
+    else
+      @items[i] = Node.new(key, value)
+      @item_count += 1
+      if @item_count.to_f / size > MAX_LOAD_FACTOR
+        resize
+      end
+    end
   end
 
   def [](key)
+    i = index(key)
+    while @items[i] && @items[i].key != key
+      i = (i + 1) % size
+    end
+    if @items[i]
+      @items[i].value
+    end
   end
 
   # Returns a unique, deterministically reproducible index into an array
   # We are hashing based on strings, let's use the ascii value of each string as
   # a starting point.
-  def index(key, size)
+  def index(key, range=size)
+    sum = 0
+    key.each_byte { |char|
+      sum += char
+    }
+    sum % range
   end
 
   # Given an index, find the next open index in @items
   def next_open_index(index)
+    while @items[index]
+      index = (index + 1) % size
+    end
+    index
   end
 
   # Simple method to return the number of items in the hash
   def size
+    @items.size
   end
 
   # Resize the hash
   def resize
+    old_items = @items
+    @items = Array.new(size * 2)
+    old_items.each { |node|
+      if node
+        @items[next_open_index(index(node.key))] = node
+      end
+    }
   end
+
+  private
+
+  MAX_LOAD_FACTOR = 0.7
 end
