ba757c56866cbb8d3ec1f32f7fad6d46d10ecc9f
Luther <lutheroto@gmail.com>
2017-01-03 04:38:15 -0500

    Fix SeparateChaining tests.

--- 01-data-structures/05-hashes-part-2/separate_chaining/node.rb
+++ 01-data-structures/05-hashes-part-2/separate_chaining/node.rb
@@ -1,8 +1,23 @@
 class Node
-  attr_accessor :next
-  attr_accessor :key
+  # We make value changeable because that's at the core of changing what a key
+  # points to. The rest is immutable. (We can't currently remove keys from the
+  # hash table.)
+  attr_reader :next
+  attr_reader :key
   attr_accessor :value
 
-  def initialize(key, value)
+  def initialize(key, value, next_)
+    @key = key
+    @value = value
+    @next = next_
+  end
+
+  # Copy a Node, but with a different next field.
+  def self.from_node node, next_
+    new(node.key, node.value, next_)
+  end
+
+  def to_s
+    "#@key: #@value => #@next"
   end
 end
--- 01-data-structures/05-hashes-part-2/separate_chaining/separate_chaining.rb
+++ 01-data-structures/05-hashes-part-2/separate_chaining/separate_chaining.rb
@@ -1,33 +1,92 @@
-require_relative 'linked_list'
+require_relative 'node'
 
 class SeparateChaining
-  attr_reader :max_load_factor
+  MAX_LOAD_FACTOR = 0.7
 
   def initialize(size)
-    @max_load_factor = 0.7
+    @items = Array.new(size)
+    @item_count = 0
   end
 
   def []=(key, value)
+    i = index(key, @items.size)
+    n = @items[i]
+    while n && n.key != key
+      n = n.next
+    end
+    if n
+      n.value = value
+    else
+      n = Node.new(key, value, @items[i])
+      @items[i] = n
+      @item_count += 1
+
+      # Resize the hash if the load factor grows too large
+      if load_factor > MAX_LOAD_FACTOR
+        resize
+      end
+    end
   end
 
   def [](key)
+    list = @items.at(index(key, @items.size))
+    while list
+      if list.key == key
+        return list.value
+      end
+      list = list.next
+    end
   end
 
   # Returns a unique, deterministically reproducible index into an array
   # We are hashing based on strings, let's use the ascii value of each string as
   # a starting point.
   def index(key, size)
+    sum = 0
+
+    key.split("").each do |char|
+      if char.ord == 0
+        next
+      end
+
+      sum = sum + char.ord
+    end
+
+    sum % size
   end
 
   # Calculate the current load factor
   def load_factor
+    @item_count / self.size.to_f
   end
 
   # Simple method to return the number of items in the hash
   def size
+    @items.size
   end
 
   # Resize the hash
   def resize
+    new_size = size*2
+    new_items = Array.new(new_size)
+    @items.each do |bucket|
+      while bucket
+        i = index(bucket.key, new_size)
+        new_items[i] = Node.from_node(bucket, new_items[i])
+        bucket = bucket.next
+      end
+    end
+
+    @items = new_items
+  end
+
+  def to_s
+    str = "SeparateChaining: load factor: #{load_factor}\n"
+    @items.each_index { |i|
+      if @items[i]
+        str += "[#{i}] #{@items[i]}\n"
+      end
+    }
+    str
   end
 end
